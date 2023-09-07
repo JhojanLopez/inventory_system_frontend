eda31cfdc9ae7b5d4cc6b652f54d11424b52f672
JhojanLopez <jhojanlopez327@gmail.com>
2023-09-07 14:40:21 -0500

    se agregan validaciones

--- src/app/components/create-merchandise/create-merchandise.component.ts
+++ src/app/components/create-merchandise/create-merchandise.component.ts
@@ -17,7 +17,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./create-merchandise.component.css'],
 })
 export class CreateMerchandiseComponent implements OnInit {
-  constructor(private fb: FormBuilder, private merchandiseService: MerchandiseService) {}
+  constructor(
+    private fb: FormBuilder,
+    private merchandiseService: MerchandiseService
+  ) {}
 
   merchandiseToCreate: MerchandiseToCreate;
   form: FormGroup;
@@ -27,43 +30,120 @@ export class CreateMerchandiseComponent implements OnInit {
     this.form = this.fb.group({
       //debemos de incializar el formulario y agregar sus inputs como formControl
       name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      amount: new FormControl('', [Validators.required, Validators.min(0)]),
+      amount: new FormControl('', [
+        Validators.required,
+        Validators.min(0),
+        this.amountValidator,
+      ]),
       dateEntry: new FormControl('', [Validators.required, this.dateValidator]),
     });
   }
-  
-  dateValidator(control: AbstractControl): ValidationErrors | null {
-    return null;
-  }
 
   onSubmit() {
-    console.log(this.form.value);
     this.merchandiseToCreate = this.form.value;
     this.merchandiseToCreate.registeredById = 1;
-  
+
     console.log(this.merchandiseToCreate);
 
     this.merchandiseService.create(this.merchandiseToCreate).subscribe({
-      next:(r)=>{
+      next: (r) => {
         console.log(r);
         Swal.fire('Exito:', `Se ha creado la mercancia con exito!`, 'success');
         this.refresh();
       },
-      error: (e)=>{
+      error: (e) => {
         console.log(e);
-        Swal.fire('Error:', `No se pudo crear la mercancia, por fallas en nuestros servidores`, 'error');
-      }
+        Swal.fire(
+          'Error:',
+          `Ya existe una mercancia con el mismo nombre, por favor escoge otro`,
+          'error'
+        );
+      },
     });
   }
 
-  refresh(){
-    console.log(this.merchandiseToCreate);
-    this.merchandiseToCreate.name='';
+  refresh() {
+    this.merchandiseToCreate.name = '';
     this.merchandiseToCreate.amount = null;
     this.merchandiseToCreate.dateEntry = null;
     this.merchandiseToCreate.registeredById = null;
-    console.log(this.merchandiseToCreate);
     this.form.reset();
-    console.log(this.form);
+  }
+
+  getFormControls(): any {
+    return this.form.controls;
+  }
+
+  amountValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (!Number.isInteger(value)) {
+      return { error: true };
+    }
+    return null;
+  }
+
+  dateValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    const dateEntry = new Date(value);
+    const currentDate: Date = new Date();
+
+    if (dateEntry > currentDate) return { error: true };
+    return null;
+  }
+
+  fieldValidator(field: string): boolean {
+    const formControl = this.getFormControls();
+
+    if (this.form.get(field).touched && this.form.get(field).errors)
+      return true;
+
+    return false;
+  }
+
+  getError(field: string): string {
+    switch (field) {
+      case 'name':
+        return this.getErrorName();
+      case 'amount':
+        return this.getErrorAmount();
+      case 'dateEntry':
+        return this.getErrorDate();
+      default:
+        return 'mensaje de error no encontrado';
+    }
+  }
+
+  getErrorName(): string {
+    const field = 'name';
+    const formControl = this.getFormControls();
+
+    //importante que el formcontrol tenga el mismo nombre y sus errores deben de ser acordes
+    if (formControl[field].errors['required']) return 'El campo es requerido';
+
+    const minLength = formControl[field].errors['minlength'].requiredLength;
+    return `El campo debe tener minimo ${minLength} caracteres`;
+  }
+
+  getErrorAmount(): string {
+    const field = 'amount';
+    const formControl = this.getFormControls();
+
+    //importante que el formcontrol tenga el mismo nombre y sus errores deben de ser acordes
+    if (formControl[field].errors['required']) return 'El campo es requerido';
+
+    if (formControl[field].errors['min'])
+      return 'El campo debe tener un valor minimo de 0';
+
+    return 'El campo debe ser un numero entero';
+  }
+
+  getErrorDate(): string {
+    const field = 'dateEntry';
+    const formControl = this.getFormControls();
+
+    //importante que el formcontrol tenga el mismo nombre y sus errores deben de ser acordes
+    if (formControl[field].errors['required']) return 'El campo es requerido';
+
+    return 'La fecha de ingreso no puede ser posterior a la fecha actual';
   }
 }
